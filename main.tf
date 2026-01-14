30b01508ffa37753f95b78c0d5660ff4c571aadf
gibokNA <skrlqhr199711@gmail.com>
2026-01-14 20:08:10 +0900

    setup remote backend with s3 and dynamoDB

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
 # terraform 실습시작
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-gibok-2026"
+    key = "terraform/state.tfstate"
+    region = "ap-northeast-2"
+    encrypt = true
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
